3aeea83c60ede051ed0de8c9c9a1768f63f5c062
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2022-07-01 13:33:00 +0530

    metadata ui

--- metadatacockpit/webapp/controller/Main.controller.js
+++ metadatacockpit/webapp/controller/Main.controller.js
@@ -30,6 +30,27 @@ sap.ui.define([
                         return "Critical";
                     }
                 }
-            }
+            },
+            onUpdateMetaData:function(oEvent){
+                debugger;
+                var oPaths=oEvent.getSource().getParent().getParent().getSelectedContextPaths();
+                var oSchemas=[];
+                for (let index = 0; index < oPaths.length; index++) {
+                    const element = oPaths[index];
+                    var oData=this.getView().getModel("local").getProperty(element);
+                    oSchemas.push(oData.dbName);
+                }
+                if(oSchemas.length>0){
+                    var that=this;
+                    var oPayload=JSON.stringify(oSchemas);
+			        this.middleWare.callMiddleWare("/UpdateMetadata?Schema="+oPayload, "GET", {})
+					.then(function (data, status, xhr) {
+						debugger;
+					})
+					.catch(function (jqXhr, textStatus, errorMessage) {
+						that.middleWare.errorHandler(jqXhr,that);
+					});
+                }
+            },
         });
     });
